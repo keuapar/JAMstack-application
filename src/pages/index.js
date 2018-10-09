5a07d7cb37aa0bf889adaf030a6442e9e2b8c910
keuapar <mikulas.plesak@gmail.com>
2018-10-08 23:41:27 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div class="flexcontainer">
+    <h1>Hello to everyone checking my application!</h1>
+    <p>I admit my experience with node.js and Gatsby is pretty limited. You could probably guess that from the look of this webpage though right.</p>
+    <p>I know my way around html and css, but only when I build it myself and so know what does what, which is not exactly the case here duh</p>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
